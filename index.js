4b58a65d1183f42adb5a80d0b1fcdd954af9acad
jarin96 <2017-1-60-027@std.ewubd.edu>
2022-05-03 02:48:48 +0600

    backend delete part done

--- index.js
+++ index.js
@@ -44,6 +44,18 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const result = await inventoryCollection.deleteOne(query);
             res.send(result);
+            // console.log(req.params + 'rentiger');
+        });
+        app.get('/item/:email', async (req, res) => {
+            // const email = req.params.email;
+            // console.log(req);
+            const user = {
+                email: req.params.email
+            }
+            const result = await inventoryCollection.find(user).toArray();
+            console.log(result);
+            res.send(result);
+            // console.log(req.params.email);
         });
 
     }
